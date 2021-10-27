ec331afe5d0a1b54290da05e9dc4b02af980b487
Ayça ege Kayhan <aycaegekayhan@Aycas-MacBook-Air.local>
2021-10-27 10:10:42 -0400

    initial commit

---
+++ weatherapi/weatherapi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherapi
+//
+//  Created by Ayça ege Kayhan on 10/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherapi/weatherapi/weatherapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherapiApp.swift
+//  weatherapi
+//
+//  Created by Ayça ege Kayhan on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct weatherapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
